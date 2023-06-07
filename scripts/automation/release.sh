3ea848189ca17cd0bfe9864497d5f40ffefbaaa5
degenaro <lou.degenaro@gmail.com>
2023-06-07 12:09:37 -0400

    WIP
    Signed-off-by: degenaro <lou.degenaro@gmail.com>

--- scripts/automation/release.sh
+++ scripts/automation/release.sh
@@ -6,6 +6,13 @@ version_tag=$(semantic-release print-version)
 echo "Bumping version of profiles to ${version_tag}" 
 export VERSION_TAG="$version_tag"
 echo "VERSION_TAG=${VERSION_TAG}" >> $GITHUB_ENV
+
+COUNT=$(ls -l md_catalogs | grep ^- | wc -l)
+if [ $COUNT -lt 1 ]
+then
+	./scripts/automation/regenerate_catalogs.sh $version_tag
+fi
+
 ./scripts/automation/assemble_catalogs.sh $version_tag
 git config --global user.email "$EMAIL"
 git config --global user.name "Automation Bot" 
